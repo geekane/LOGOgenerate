e4f300439876e475c5ba48c55686b07cf58d2ce5
geekane <32669380+geekane@users.noreply.github.com>
2025-01-13 23:07:17 +0800

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,58 @@
+import type { Metadata } from "next";
+import { Jura } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "@/app/components/ui/toaster";
+
+const jura = Jura({
+  subsets: ["latin"],
+  variable: "--font-jura",
+});
+
+const title = "Logo-creator.io – Generate a logo";
+const description = "Generate a logo for your company";
+const url = "https://www.logo-creator.io/";
+const ogimage = "https://www.logo-creator.io/og-image.png";
+const sitename = "logo-creator.io";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(url),
+  title,
+  description,
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    images: [ogimage],
+    title,
+    description,
+    url: url,
+    siteName: sitename,
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: [ogimage],
+    title,
+    description,
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full">
+      <head>
+         <meta name="color-scheme" content="dark" />
+      </head>
+      <body
+        className={`${jura.variable} dark min-h-full bg-[#343434] font-jura antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,296 @@
+"use client";
+
+import Spinner from "@/app/components/Spinner";
+import { Button } from "@/app/components/ui/button";
+import { Input } from "@/app/components/ui/input";
+import { Textarea } from "@/app/components/ui/textarea";
+import { motion } from "framer-motion";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import * as RadioGroup from "@radix-ui/react-radio-group";
+import { DownloadIcon, RefreshCwIcon } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { domain } from "@/app/lib/domain";
+
+const logoStyles = [
+  { name: "科技", icon: "/tech.svg" },
+  { name: "炫彩", icon: "/flashy.svg" },
+  { name: "现代", icon: "/modern.svg" },
+  { name: "趣味", icon: "/playful.svg" },
+  { name: "抽象", icon: "/abstract.svg" },
+  { name: "简约", icon: "/minimal.svg" },
+];
+
+const primaryColors = [
+  { name: "蓝色", color: "#0F6FFF" },
+  { name: "红色", color: "#FF0000" },
+  { name: "绿色", color: "#00FF00" },
+  { name: "黄色", color: "#FFFF00" },
+];
+
+const backgroundColors = [
+  { name: "白色", color: "#FFFFFF" },
+  { name: "灰色", color: "#CCCCCC" },
+  { name: "黑色", color: "#000000" },
+];
+
+export default function Page() {
+  const [companyName, setCompanyName] = useState("");
+  const [selectedStyle, setSelectedStyle] = useState(logoStyles[0].name);
+  const [selectedPrimaryColor, setSelectedPrimaryColor] = useState(
+    primaryColors[0].name,
+  );
+  const [selectedBackgroundColor, setSelectedBackgroundColor] = useState(
+    backgroundColors[0].name,
+  );
+  const [additionalInfo, setAdditionalInfo] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [generatedImage, setGeneratedImage] = useState("");
+
+  async function generateLogo() {
+    setIsLoading(true);
+
+    const res = await fetch("/api/generate-logo", {
+      method: "POST",
+      body: JSON.stringify({
+        companyName,
+        selectedStyle,
+        selectedPrimaryColor,
+        selectedBackgroundColor,
+        additionalInfo,
+      }),
+    });
+
+    if (res.ok) {
+      const json = await res.json() as any;
+      setGeneratedImage(json.url);
+    } else {
+      alert(`Error: ${res.statusText} `);
+    }
+    setIsLoading(false);
+  }
+
+  return (
+    <div className="flex h-screen flex-col overflow-y-auto overflow-x-hidden bg-[#343434] md:flex-row">
+      <Header className="block md:hidden" />
+
+      <div className="flex w-full flex-col md:flex-row">
+        <div className="relative flex h-full w-full flex-col bg-[#2C2C2C] text-[#F3F3F3] md:max-w-sm">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              setGeneratedImage("");
+              generateLogo();
+            }}
+            className="flex h-full w-full flex-col"
+          >
+            <fieldset className="flex grow flex-col">
+              <div className="flex-grow overflow-y-auto">
+                <div className="px-8 pb-0 pt-4 md:px-6 md:pt-6">
+                  <div className="mb-6">
+                    <label
+                      htmlFor="company-name"
+                      className="mb-2 block text-xs font-bold uppercase text-[#6F6F6F]"
+                    >
+                     公司名称
+                    </label>
+                    <Input
+                      value={companyName}
+                      onChange={(e) => setCompanyName(e.target.value)}
+                      placeholder="例如：Sam's Burgers"
+                      required
+                    />
+                  </div>
+
+                  <div className="mb-6">
+                    <label className="mb-2 flex items-center text-xs font-bold uppercase text-[#6F6F6F]">
+                      风格
+                    </label>
+                    <RadioGroup.Root
+                      value={selectedStyle}
+                      onValueChange={setSelectedStyle}
+                      className="grid grid-cols-3 gap-3"
+                    >
+                      {logoStyles.map((logoStyle) => (
+                        <RadioGroup.Item
+                          value={logoStyle.name}
+                          key={logoStyle.name}
+                          className="group text-[#6F6F6F] focus-visible:outline-none data-[state=checked]:text-white"
+                        >
+                          <Image
+                            src={logoStyle.icon}
+                            alt={logoStyle.name}
+                            width={96}
+                            height={96}
+                            className="w-full rounded-md border border-transparent group-focus-visible:outline group-focus-visible:outline-offset-2 group-focus-visible:outline-gray-400 group-data-[state=checked]:border-white"
+                          />
+                          <span className="text-xs">{logoStyle.name}</span>
+                        </RadioGroup.Item>
+                      ))}
+                    </RadioGroup.Root>
+                  </div>
+
+                  <div className="mb-[25px] flex flex-col md:flex-row md:space-x-3">
+                    <div className="mb-4 flex-1 md:mb-0">
+                      <label className="mb-1 block text-xs font-bold uppercase text-[#6F6F6F]">
+                       主要颜色
+                      </label>
+                      <Select
+                        value={selectedPrimaryColor}
+                        onValueChange={setSelectedPrimaryColor}
+                      >
+                        <SelectTrigger>
+                          <SelectValue placeholder="选择颜色" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectGroup>
+                            {primaryColors.map((color) => (
+                              <SelectItem key={color.color} value={color.name}>
+                                <span className="flex items-center">
+                                  <span
+                                    style={{ backgroundColor: color.color }}
+                                    className="mr-2 size-4 rounded-sm bg-white"
+                                  />
+                                  {color.name}
+                                </span>
+                              </SelectItem>
+                            ))}
+                          </SelectGroup>
+                        </SelectContent>
+                      </Select>
+                    </div>
+                    <div className="flex-1">
+                      <label className="mb-1 block items-center text-xs font-bold uppercase text-[#6F6F6F]">
+                        背景色
+                      </label>
+                      <Select
+                        value={selectedBackgroundColor}
+                        onValueChange={setSelectedBackgroundColor}
+                      >
+                        <SelectTrigger>
+                          <SelectValue placeholder="选择颜色" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectGroup>
+                            {backgroundColors.map((color) => (
+                              <SelectItem key={color.color} value={color.name}>
+                                <span className="flex items-center">
+                                  <span
+                                    style={{ backgroundColor: color.color }}
+                                    className="mr-2 size-4 rounded-sm bg-white"
+                                  />
+                                  {color.name}
+                                </span>
+                              </SelectItem>
+                            ))}
+                          </SelectGroup>
+                        </SelectContent>
+                      </Select>
+                    </div>
+                  </div>
+                  <div className="mb-1">
+                    <div className="mt-1">
+                      <div className="mb-1">
+                        <label
+                          htmlFor="additional-info"
+                          className="mb-2 flex items-center text-xs font-bold uppercase text-[#6F6F6F]"
+                        >
+                         附加信息
+                        </label>
+                        <Textarea
+                          value={additionalInfo}
+                          onChange={(e) => setAdditionalInfo(e.target.value)}
+                          placeholder="输入关于你的logo的其他信息"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="px-8 py-4 md:px-6 md:py-6">
+                <Button
+                  size="lg"
+                  className="w-full text-base font-bold"
+                  type="submit"
+                  disabled={isLoading}
+                >
+                  {isLoading ? (
+                    <div className="loader mr-2" />
+                  ) : (
+                    <Image
+                      src="/generate-icon.svg"
+                      alt="Generate Icon"
+                      width={16}
+                      height={16}
+                      className="mr-2"
+                    />
+                  )}
+                  {isLoading ? "生成中..." : "生成Logo"}
+                </Button>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+
+        <div className="flex w-full flex-col pt-12 md:pt-0">
+          <Header className="hidden md:block" />
+           <div className="relative flex flex-grow items-center justify-center px-4">
+            <div className="relative aspect-square w-full max-w-lg">
+              {generatedImage ? (
+                <>
+                  <Image
+                    className={`${isLoading ? "animate-pulse" : ""}`}
+                    width={512}
+                    height={512}
+                    src={generatedImage}
+                    alt="Generated logo"
+                  />
+                  <div
+                    className={`pointer-events-none absolute inset-0 transition ${isLoading ? "bg-black/50 duration-500" : "bg-black/0 duration-0"}`}
+                  />
+
+                  <div className="absolute -right-12 top-0 flex flex-col gap-2">
+                    <Button size="icon" variant="secondary" asChild>
+                      <a href={generatedImage} download="logo.png">
+                        <DownloadIcon />
+                      </a>
+                    </Button>
+                    <Button
+                      size="icon"
+                      onClick={generateLogo}
+                      variant="secondary"
+                    >
+                      <Spinner loading={isLoading}>
+                        <RefreshCwIcon />
+                      </Spinner>
+                    </Button>
+                  </div>
+                </>
+              ) : (
+                <Spinner loading={isLoading} className="size-8 text-white">
+                  <div className="flex aspect-square w-full flex-col items-center justify-center rounded-xl bg-[#2C2C2C]">
+                    <h4 className="text-center text-base leading-tight text-white">
+                      在10秒内
+                      <br />
+                     生成你的专属 Logo
+                    </h4>
+                  </div>
+                </Spinner>
+              )}
+            </div>
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+}
