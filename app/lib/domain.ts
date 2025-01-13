0ff43c45052d2383300392763ef1dfb535a8ba9b
geekane <32669380+geekane@users.noreply.github.com>
2025-01-13 23:18:28 +0800

    Create domain.ts

---
+++ app/lib/domain.ts
@@ -0,0 +1,8 @@
+export const domain =
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+    ? "https://www.logo-creator.io"
+    : process.env.VERCEL_BRANCH_URL
+      ? `https://${process.env.VERCEL_BRANCH_URL}`
+      : process.env.NEXT_PUBLIC_VERCEL_URL
+        ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+        : "http://localhost:3000";
